a45b89e0be7636141a22c23578e210bc6e97ceb6
Tomas Diaco <tomel95@hotmail.com>
2021-05-18 21:41:38 +0400

    fixing reviews

--- pages/business/[id].js
+++ pages/business/[id].js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
-import { isNil } from "ramda";
+import { isEmpty, isNil } from "ramda";
 import { useLazyQuery } from "@apollo/client";
 import moment from "moment";
 
@@ -105,7 +105,7 @@ function BusinessDetail({ businessList }) {
     photos: "",
     rating: "",
     review_count: "",
-    reviews: "",
+    reviews: [],
     price: "",
   });
 
@@ -189,24 +189,26 @@ function BusinessDetail({ businessList }) {
         </ContactNumber>
         <Cost>Cost of prices: {price}</Cost>
       </Description>
-      <Reviews>
-        {reviews.map((review, index) => {
-          return (
-            <ReviewItem key={`review-${index}`}>
-              <Rating>
-                {review.rating}
-                <svg className="svg-icon" viewBox="0 0 20 20">
-                  <path
-                    fill="none"
-                    d="M16.85,7.275l-3.967-0.577l-1.773-3.593c-0.208-0.423-0.639-0.69-1.11-0.69s-0.902,0.267-1.11,0.69L7.116,6.699L3.148,7.275c-0.466,0.068-0.854,0.394-1,0.842c-0.145,0.448-0.023,0.941,0.314,1.27l2.871,2.799l-0.677,3.951c-0.08,0.464,0.112,0.934,0.493,1.211c0.217,0.156,0.472,0.236,0.728,0.236c0.197,0,0.396-0.048,0.577-0.143l3.547-1.864l3.548,1.864c0.18,0.095,0.381,0.143,0.576,0.143c0.256,0,0.512-0.08,0.729-0.236c0.381-0.277,0.572-0.747,0.492-1.211l-0.678-3.951l2.871-2.799c0.338-0.329,0.459-0.821,0.314-1.27C17.705,7.669,17.316,7.343,16.85,7.275z M13.336,11.754l0.787,4.591l-4.124-2.167l-4.124,2.167l0.788-4.591L3.326,8.5l4.612-0.67l2.062-4.177l2.062,4.177l4.613,0.67L13.336,11.754z"
-                  ></path>
-                </svg>
-              </Rating>
-              <ReviewText>{review.text}</ReviewText>
-            </ReviewItem>
-          );
-        })}
-      </Reviews>
+      {!isEmpty(reviews) && !isNil(reviews) && (
+        <Reviews>
+          {reviews.map((review, index) => {
+            return (
+              <ReviewItem key={`review-${index}`}>
+                <Rating>
+                  {review.rating}
+                  <svg className="svg-icon" viewBox="0 0 20 20">
+                    <path
+                      fill="none"
+                      d="M16.85,7.275l-3.967-0.577l-1.773-3.593c-0.208-0.423-0.639-0.69-1.11-0.69s-0.902,0.267-1.11,0.69L7.116,6.699L3.148,7.275c-0.466,0.068-0.854,0.394-1,0.842c-0.145,0.448-0.023,0.941,0.314,1.27l2.871,2.799l-0.677,3.951c-0.08,0.464,0.112,0.934,0.493,1.211c0.217,0.156,0.472,0.236,0.728,0.236c0.197,0,0.396-0.048,0.577-0.143l3.547-1.864l3.548,1.864c0.18,0.095,0.381,0.143,0.576,0.143c0.256,0,0.512-0.08,0.729-0.236c0.381-0.277,0.572-0.747,0.492-1.211l-0.678-3.951l2.871-2.799c0.338-0.329,0.459-0.821,0.314-1.27C17.705,7.669,17.316,7.343,16.85,7.275z M13.336,11.754l0.787,4.591l-4.124-2.167l-4.124,2.167l0.788-4.591L3.326,8.5l4.612-0.67l2.062-4.177l2.062,4.177l4.613,0.67L13.336,11.754z"
+                    ></path>
+                  </svg>
+                </Rating>
+                <ReviewText>{review.text}</ReviewText>
+              </ReviewItem>
+            );
+          })}
+        </Reviews>
+      )}
     </BusinessDetailContainer>
   );
 }
