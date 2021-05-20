c81eb750f62587a2985b7cf517a0650e84cbd663
Tomas Diaco <tomel95@hotmail.com>
2021-05-20 19:58:26 +0400

    meta tag fix

--- pages/_document.js
+++ pages/_document.js
@@ -21,11 +21,11 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,400;0,700;0,800;1,300&display=swap"
             rel="stylesheet"
           ></link>
+          {this.props.styleTags}
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, maximum-scale=1"
-          ></meta>
-          {this.props.styleTags}
+          />
         </Head>
         <body>
           <Main />
