815309ba787fb8aca1463719e2fe1235595e6660
Tomas Diaco <tomel95@hotmail.com>
2021-05-20 19:06:33 +0400

    quick fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         // mathching all API routes
-        source: "https://api.yelp.com/v3/graphql",
+        source: "/pages/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
